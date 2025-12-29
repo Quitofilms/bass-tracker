5cba2767445ab89a53144d3187fc7a2adcc33a75
Quitofilms <53586207+Quitofilms@users.noreply.github.com>
2025-12-29 20:18:36 +1000

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,40 @@
+const CACHE_NAME = 'bass-tracker-v1.4';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json'
+  // If you add icons later, uncomment these lines:
+  // './icon-192.png',
+  // './icon-512.png'
+];
+
+// Install Event: Cache files
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Activate Event: Clean up old caches
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+// Fetch Event: Serve from Cache, fall back to Network
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
